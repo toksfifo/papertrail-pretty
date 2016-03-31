b215d931cc03b6ae0c3ba4361e555815fe6e48a5
Toks Fifo <toksfifo@gmail.com>
2016-03-31 15:48:03 -0400

    syntax highlighting progress

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,8 +3,19 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
 
-gulp.task('default', () => {
-  gulp.src('src/*')
-    // .pipe(babel({ presets: ['es2015'] }))
+gulp.task('default', ['copy', 'js']);
+
+gulp.task('watch', () => {
+  gulp.watch('src/app.js', ['js']);
+});
+
+gulp.task('copy', () => {
+  gulp.src(['src/*', '!src/app.js'])
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('js', () => {
+  gulp.src('src/app.js')
+    .pipe(babel({ presets: ['es2015'] }))
     .pipe(gulp.dest('dist'));
 });
