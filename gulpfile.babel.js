494a7686dd928b0026c7d14c0f6a3388c8992af7
Toks Fifo <toksfifo@gmail.com>
2016-04-03 14:40:38 -0400

    re-arrange file structure to include /lib

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,15 +2,17 @@
 
 const gulp = require('gulp');
 const babel = require('gulp-babel');
+const del = require('del');
 
-gulp.task('default', ['copy', 'js']);
+gulp.task('default', ['clean-dist', 'copy', 'js']);
 
+// Only watching js
 gulp.task('watch', () => {
   gulp.watch('src/app.js', ['js']);
 });
 
 gulp.task('copy', () => {
-  gulp.src(['src/*', '!src/app.js'])
+  gulp.src(['src/**/*', '!src/app.js'])
     .pipe(gulp.dest('dist'));
 });
 
@@ -18,4 +20,8 @@ gulp.task('js', () => {
   gulp.src('src/app.js')
     .pipe(babel({ presets: ['es2015'] }))
     .pipe(gulp.dest('dist'));
+});
+
+gulp.task('clean-dist', () => {
+	del(['dist/**/*']);
 });
